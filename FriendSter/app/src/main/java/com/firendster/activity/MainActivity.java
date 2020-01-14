a90c0969c1751d830c8ff95c3fcf1489b6baf45a
Salvador Silva <salvadorsilvajr@yahoo.com>
2020-01-13 22:07:02 -0600

    friendster agregar el usuario to xampp con el google sing in Ready to used

---
+++ FriendSter/app/src/main/java/com/firendster/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.firendster.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.firendster.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
