5660958c9ba29da3edc7d91771c37d52a4447ba2
Salvador Silva <salvadorsilvajr@yahoo.com>
2020-01-13 18:44:50 -0600

    friendster agregar el usuario to xampp con el google sing in

---
+++ app/src/main/java/com/friendster/activity/res/ApiClient.java
@@ -0,0 +1,26 @@
+package com.friendster.activity.res;
+
+import android.app.Activity;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient extends Activity {
+    public static final String BASE_URL ="http://192.168.1.116/friendster/public/app/";
+//    public static final String BASE_URL = "http://10.0.2.2/friendster/public/app/";
+    private static Retrofit retrofit= null;
+
+  public static Retrofit getApiClient(){
+      HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+      OkHttpClient httpClient = new OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build();
+      if(retrofit==null) {
+          retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
+                  .client(httpClient)
+                  .addConverterFactory(GsonConverterFactory.create())
+                  .build();
+      }
+      return retrofit;
+  }
+}
